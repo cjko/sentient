47724c9a0013f672384c5d26f7a0b00e88d063d9
Colin Jao <jao.colin@gmail.com>
2017-10-23 01:07:41 -0500

    Ran through linter and updated README.md

--- README.md
+++ README.md
@@ -1,2 +1,8 @@
 <h1>Sentient</h1>
 Twitter Sentiment Analysis. A web application that analyzes tweets and tracks whether public sentiment on a particular subject is generally positive or negative.
+
+<h2>Getting Started</h2>
+
+<h3>Prerequisites</h3>
+
+<h3>Installing</h3>
--- app.js
+++ app.js
@@ -2,7 +2,7 @@ const port = 8000;
 const express = require('express');
 const bodyparser = require('body-parser');
 const sentiment = require('sentiment');
-const tweeter = require('./config')
+const tweeter = require('./config');
 
 const app = express();
 app.use(bodyparser.json());
@@ -25,22 +25,23 @@ app.get('/tweets', (req,res)=>{
 				testTweetCount++;
 				console.log(`Tweet #${testTweetCount}: ${data.text}`);
 				sentiment(data.text, (err, result)=>{
-					totalSentiment += result.score
-					console.log(`Sentiment score so far: ${totalSentiment}`)
-				})
-			})
+					totalSentiment += result.score;
+					console.log(`Sentiment score so far: ${totalSentiment}`);
+				});
+			});
 		}
-		)
-		// res.send(`Hello ${data.name}. I am analyzing your twitter.`)
-	})
-})
+		);
+		// res.send(`Hello ${data.name}. I am analyzing your twitter.`);
+	});
+});
 
 app.get('/', (req,res)=>{
 	sentiment('westbrook',(err, result)=>{
-		const response = `Sentiment score for WESTBROOK is ${result.score}.`
+		const response = `Sentiment score for WESTBROOK is ${result.score}.`;
 		res.send(response);
 	});
 });
+
 
 app.listen(8000, ()=>{
 	console.log(`Listening on port ${port}`);
