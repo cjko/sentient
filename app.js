1ca003bb5a91f9e10e9d66db61c4e170efac3757
Colin Jao <jao.colin@gmail.com>
2017-10-28 11:23:41 -0500

    Replaced sentiment with ml-sentiment module for negated text.

--- app.js
+++ app.js
@@ -2,7 +2,15 @@ const port = 8000;
 const express = require('express');
 const bodyparser = require('body-parser');
 const sentiment = require('sentiment');
-const tweeter = require('./config');
+const ml = require('ml-sentiment')();
+const config = require('./config');
+const twitter = require('twitter');
+const tweetClient = new twitter({
+	'consumer_key': config.consumer_key,
+	'consumer_secret': config.consumer_secret,
+	'access_token_key': config.access_token_key,
+	'access_token_secret': config.access_token_secret
+});
 
 const app = express();
 app.use(bodyparser.json());
@@ -13,22 +21,33 @@ app.set('view engine', 'ejs');
 
 // Routes
 app.get('/tweets', (req,res)=>{
-	tweeter.verifyCredentials((err, data)=>{
+	tweetClient.verifyCredentials((err, data)=>{
 		let totalSentiment = 0;
 		let testTweetCount = 0;
-		const phrase = 'weinstein';
-		stream = tweeter.stream('statuses/filter', {
+		let log = ''
+		const phrase = 'blade runner 2049';
+		stream = tweetClient.stream('statuses/filter', {
 			'track': phrase
 		}, (stream)=>{
-			res.send(`Monitoring Twitter for ${phrase}...`);
+			res.send(`Monitoring Twitter for ${phrase}...\n${log}`);
 			stream.on('data', (data)=>{
-				testTweetCount++;
-				console.log(`Tweet #${testTweetCount}: ${data.text}`);
-				sentiment(data.text, (err, result)=>{
-					totalSentiment += result.score;
-					console.log(`Sentiment score so far: ${totalSentiment}`);
-				});
+				// Only evaluate the sentiment of English-language tweets
+				if (data.lang === 'en') {
+					testTweetCount++;
+					totalSentiment += ml.classify(data.text);
+					console.log(`Tweet #${testTweetCount}: ${data.text}`);
+					console.log(`Sentiment score so far: ${totalSentiment/testTweetCount}`);
+
+					/*	
+					sentiment(data.text, (err, result)=>{
+						testTweetCount++;
+						console.log(`Tweet #${testTweetCount}: ${data.text}`);
+						totalSentiment += result.score;
+						console.log(`Sentiment score so far: ${totalSentiment/testTweetCount}`);
+					});*/
+				}
 			});
+
 		}
 		);
 		// res.send(`Hello ${data.name}. I am analyzing your twitter.`);
@@ -36,10 +55,40 @@ app.get('/tweets', (req,res)=>{
 });
 
 app.get('/', (req,res)=>{
-	sentiment('westbrook',(err, result)=>{
-		const response = `Sentiment score for WESTBROOK is ${result.score}.`;
+	const phrase = 'Westbrook is a god.'
+	/*
+	sentiment(phrase,(err, result)=>{
+		const response = `Sentiment score for "${phrase}" is ${result.score}.`;
 		res.send(response);
-	});
+	});*/
+	console.log(ml.classify(phrase));
+	res.send(`${phrase} got a score of ${ml.classify(phrase)}`);
+});
+
+app.get('/search', (req,res)=>{
+	const phrase = 'bitcoin';
+	let totalSentiment = 0;
+	let testTweetCount = 0;
+	tweetClient.get('search/tweets', {q: phrase, vertical:'news',src:'typd',count:100}, (err,tweets,response)=>{
+		// console.log(tweets)
+		let parsedTweets = tweets['statuses'].map((json)=>{
+			return ml.classify(json['text']);
+		})
+		res.send(parsedTweets)
+	})
+	// res.send(`Monitoring Twitter statuses for: ${phrase}...`)
+	// tweetClient.stream('statuses/filter',{track: phrase}, (stream)=>{
+	// 	stream.on('data',(tweet)=>{
+	// 		if(tweet.lang === 'en'){
+	// 			testTweetCount++;
+	// 			totalSentiment += ml.classify(tweet.text);
+	// 			console.log(totalSentiment/testTweetCount);
+	// 		}
+	// 	})
+
+	// })
+
+	
 });
 
 
